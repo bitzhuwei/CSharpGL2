2b2a407bd07ab5fb83371b0062847dbd7d6ef616
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-18 23:36:56 +0800

    namespace

--- CSharpGL/OpenGLObjects/ModernRendering/ShaderCode.cs
+++ CSharpGL/OpenGLObjects/ModernRendering/ShaderCode.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace CSharpGL.GLObjects.ModernRendering
+namespace CSharpGL
 {
     /// <summary>
     /// 各种类型的shader代码
