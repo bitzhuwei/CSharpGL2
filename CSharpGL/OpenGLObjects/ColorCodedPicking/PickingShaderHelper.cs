4f8fe1a8afb54d5b5792e904ff5ccbc3af6a6bf6
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-19 19:26:01 +0800

    ColorCodedPicking

---
+++ CSharpGL/OpenGLObjects/ColorCodedPicking/PickingShaderHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpGL
+{
+    /// <summary>
+    /// Description of PickingShaderProgram
+    /// </summary>
+    public static class PickingShaderHelper //: IDisposable
+    {
+
+        public static ShaderProgram GetPickingShaderProgram()
+        {
+            ShaderCode[] shaders = new ShaderCode[2];
+            shaders[0] = new ShaderCode(GetShaderSource(ShaderType.VertexShader), ShaderType.VertexShader);
+            shaders[1] = new ShaderCode(GetShaderSource(ShaderType.FragmentShader), ShaderType.VertexShader);
+
+            var shaderProgram = new ShaderProgram();
+            shaderProgram.Create((from item in shaders select item.CreateShader()).ToArray());
+
+            return shaderProgram;
+        }
+
+        //static readonly object synObj = new object();
+        //private static ShaderProgram shaderProgram = null;
+
+        //static PickingShaderProgram()
+        //{
+        //if (shaderProgram == null)
+        //{
+        //    lock (synObj)
+        //    {
+        //        if (shaderProgram == null)
+        //        {
+        //            var vertexShaderSource = ManifestResourceLoader.LoadTextFile(@"PickingShader.vert");
+        //            string fragmentShaderSource = ManifestResourceLoader.LoadTextFile(@"PickingShader.frag");
+
+        //            shaderProgram = new ShaderProgram();
+        //            shaderProgram.Create(vertexShaderSource, fragmentShaderSource, null);
+
+        //            shaderProgram.AssertValid();
+        //        }
+        //    }
+        //}
+        //}
+
+        /// <summary>
+        /// vertex shader's cache.
+        /// </summary>
+        static string vertexShader = null;
+
+        /// <summary>
+        /// fragmente shader's cache.
+        /// </summary>
+        static string fragmentShader = null;
+
+        /// <summary>
+        /// Gets shader's source code for color coded picking.
+        /// </summary>
+        /// <param name="shaderType"></param>
+        /// <returns></returns>
+        public static string GetShaderSource(ShaderType shaderType)
+        {
+            string result = string.Empty;
+
+            switch (shaderType)
+            {
+                case ShaderType.VertexShader:
+                    if (vertexShader == null)
+                    {
+                        vertexShader = ManifestResourceLoader.LoadTextFile(@"PickingShader.vert");
+                    }
+                    result = vertexShader;
+                    break;
+                case ShaderType.FragmentShader:
+                    if (fragmentShader == null)
+                    {
+                        fragmentShader = ManifestResourceLoader.LoadTextFile(@"PickingShader.frag");
+                    }
+                    result = fragmentShader;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return result;
+        }
+
+    }
+}
