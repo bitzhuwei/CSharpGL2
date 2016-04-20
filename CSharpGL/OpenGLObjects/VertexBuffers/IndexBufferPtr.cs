bceec25205ca4f1755dd07d7a913c6641f58ba75
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-20 21:42:43 +0800

    IndexBufferPtr

--- CSharpGL/OpenGLObjects/VertexBuffers/IndexBufferPtr.cs
+++ CSharpGL/OpenGLObjects/VertexBuffers/IndexBufferPtr.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,14 @@ namespace CSharpGL
     /// <summary>
     /// 索引buffer渲染器的基类。
     /// </summary>
+    [Browsable(true)]
     public abstract class IndexBufferPtr : BufferPtr
     {
 
         /// <summary>
         /// 用哪种方式渲染各个顶点？（OpenGL.GL_TRIANGLES etc.）
         /// </summary>
-        public DrawMode Mode { get; private set; }
+        public DrawMode Mode { get; set; }
 
         /// <summary>
         /// 索引buffer渲染器的基类。
