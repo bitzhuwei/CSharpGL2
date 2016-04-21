8220be1fb13a56b9fd9d06d89da427a8e018d07a
祝威 <bitzhuwei.cnblogs@qq.com>
2016-04-21 11:32:59 +0800

    PointSizeSwitch

---
+++ CSharpGL/OpenGLObjects/GLSwitches/PointSizeSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGL
+{
+    public class PointSizeSwitch : GLSwitch
+    {
+
+        public float PointSize { get; set; }
+
+        public PointSizeSwitch()
+        {
+            this.PointSize = 1.0f;
+        }
+
+        float[] original = new float[1];
+
+        public override void On()
+        {
+            GL.GetFloat(GetTarget.PointSize, original);
+
+            GL.PointSize(PointSize);
+        }
+
+        public override void Off()
+        {
+            GL.PointSize(original[0]);
+        }
+    }
+}
